20ac48f607536b69de4018be7b1ef75122e64030
Jelmer Cormont <jelmer@cormont.work>
2024-06-21 12:09:41 +0800

    Remove wrong alignment for button and link spans in CSS (#88)

--- packages/frame-web/src/style/defaults/css.ts
+++ packages/frame-web/src/style/defaults/css.ts
@@ -123,12 +123,6 @@ export function makeBaseCSS() {
 			top: 0,
 			height: "100%",
 		},
-		[`a.${CLASS_UI} span`]: {
-			verticalAlign: "middle",
-		},
-		[`button.${CLASS_UI} span`]: {
-			verticalAlign: "middle",
-		},
 
 		// flip icons in RTL mode
 		[`[dir="rtl"] ._RTL-flip`]: {
