267dce32394c3e6761f71b77e4f8421169291823
Jelmer Cormont <jelmer@cormont.work>
2025-01-14 00:13:51 +0800

    Fix: correct icon size for search icon

--- packages/web-handler/src/style/defaults/icons.ts
+++ packages/web-handler/src/style/defaults/icons.ts
@@ -1,18 +1,18 @@
 import { UIIconResource } from "talla-ui";
 
-function _mk(name: string, content: string, mirror?: boolean) {
+function _mk(name: string, content: string, mirror = false, size = 48) {
 	return [
 		name,
 		new UIIconResource(
 			name,
-			`<svg viewBox="0 0 48 48">` + content + `</svg>`,
-		).setMirrorRTL(mirror || false),
+			`<svg viewBox="0 0 ${size} ${size}">` + content + `</svg>`,
+		).setMirrorRTL(mirror),
 	] as const;
 }
 
-/** @internal SVG icon set */
+/** @internal simple SVG icon set */
 export const icons = [
-	_mk("Blank", `<svg viewBox="0 0 48 48"></svg>`),
+	_mk("Blank", ""),
 	_mk(
 		"Close",
 		`<path d="M39 12l-3-3l-12 12l-12-12l-3 3l12 12l-12 12l3 3l12-12l12 12l3-3l-12-12l12-12z"/>`,
@@ -32,5 +32,7 @@ export const icons = [
 	_mk(
 		"Search",
 		`<path d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14"/>`,
+		false,
+		24,
 	),
 ];
